18d3bcfcbb66b9342570a4a585deb8c9640f26cc
cwissell <51326406+cwissell@users.noreply.github.com>
2019-08-10 15:39:06 -0400

    backend done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Facts = () => {
+  return <h1>Hi from Facts!</h1>
+}
+
+let component;
+if (document.location.pathname === "/facts") {
+  component = <Facts />;
+} else {
+  component = <App />
+}
+
+ReactDOM.render(component, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
